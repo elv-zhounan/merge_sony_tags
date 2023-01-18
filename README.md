72a9e830c2dfc1366ebdd531f3e09e1d3010d9ce
elv-zhounan <zhounan@eluv.io>
2023-01-18 13:20:45 -0800

    fix some issues in translate file, met from format issue in xml file, may need to discuss

--- README.md
+++ README.md
@@ -3,14 +3,21 @@
 ## tasks done
 
 - [x] download tags from fabric
-- [x] translate from xml to json
 - [x] merge to shot tags
 - [x] a few analysis
+- [] translate from xml to json
 
 ## need to discuss
 
-1. what features we would like to add
-2. for search, seems like we are only using shot_tags tracks, can we just save shot_tags to make files smaller?
-3. tags from sony is covering too many shots, which may not be a good case
-4. sony xml format is not good, json files given by marc is good. are those json files from sony or marc?
+1. sony xml format is not good, json files given by marc is good. are those json files from sony or marc?
+   1. some timestamps are empty
+      - []
+      - [00:00:00:00-00:00:00:00]
+   2. sime timestamps are with bad format,for example
+      - [00:04:22:21-00:04:26;10/1.0]
+      - [ [01:14:09:00-01:18:48:09]
+      - not sure if there are any other special cases
+2. what features we would like to add
+3. for search, seems like we are only using shot_tags tracks, can we just save shot_tags to make files smaller?
+4. tags from sony is covering too many shots, which may not be a good case
 5. maybe more analysis
--- translate.py
+++ translate.py
@@ -2,6 +2,8 @@
 
 import xml.etree.ElementTree as E
 import json
+import os
+import tqdm
 
 def translate(src:str, dst:str=None):
     tree = E.parse(src)
@@ -31,40 +33,52 @@ def translate(src:str, dst:str=None):
                     content = txt[:s_next]
                     txt = txt[s_next: ]
                 
-                # TODO not sure what ss means 
-                h, m, s, ss = timetxt.split("-")[0].split(":")
-                start_time = (3600*int(h) + 60*int(m) + int(s))*1000 + int(int(ss)/24*1000)
-                h, m, s, ss = timetxt.split("-")[1].split(":")
-                end_time = (3600*int(h) + 60*int(m) + int(s))*1000 + int(int(ss)/24*1000)
+                # TODO some format issue in xml tags from sony
+                if len(timetxt) > 0 and timetxt != "00:00:00:00-00:00:00:00":
+                    while timetxt[0] > "9" or timetxt[0] < "0":
+                        timetxt = timetxt[1:]
+                    timetxt = timetxt[:23].replace(";", ":")
+                    h, m, s, ss = timetxt.split("-")[0].split(":")
+                    start_time = (3600*int(h) + 60*int(m) + int(s))*1000 + int(int(ss)/24*1000)
+                    h, m, s, ss = timetxt.split("-")[1].split(":")
+                    end_time = (3600*int(h) + 60*int(m) + int(s))*1000 + int(int(ss)/24*1000)
 
-                if key == "transcript_timeline":
-                    if not content.startswith(" -"):
-                        tags.append({
-                            "start": start_time,
-                            "end": end_time,
-                            "text":[content.strip()]
-                        })
+                    if key == "transcript_timeline":
+                        if not content.startswith(" -"):
+                            tags.append({
+                                "start": start_time,
+                                "end": end_time,
+                                "text":[content.strip()]
+                            })
+                        else:
+                            content = [c.strip() for c in content.split(" -")[1:]]
+                            tags.append({
+                                "start": start_time,
+                                "end": end_time,
+                                "text":content
+                            })
                     else:
-                        content = [c.strip() for c in content.split(" -")[1:]]
+                        content = [c.strip() for c in content.split(";")]
                         tags.append({
-                            "start": start_time,
-                            "end": end_time,
-                            "text":content
-                        })
-                else:
-                    content = [c.strip() for c in content.split(";")]
-                    tags.append({
-                            "start": start_time,
-                            "end": end_time,
-                            "text":content
-                        })
+                                "start": start_time,
+                                "end": end_time,
+                                "text":content
+                            })
                 if s_next == -1 or txt.strip() == "":
                     break
             res["metadata_tags"][key] = {
                 "label": label,
                 "tags": tags
             }
-    
-    json.dump(res, open("test_translate.json", "w"))
+
+    json.dump(res, open(dst, "w"))
+
 if __name__ == "__main__":
-    translate('./sony_tags_xml/845138_s01_Ghosbusters_1984.xml')
+    # translate('./sony_tags_xml/845138_s01_Ghosbusters_1984.xml')
+    objects = sorted(os.listdir("sony_tags_xml"))
+    if ".DS_Store" in objects:
+        objects.remove(".DS_Store")
+
+    for object in tqdm.tqdm(objects):
+        print(object)
+        translate(f"./sony_tags_xml/{object}", f"./translated_tags_json/{object.replace('.xml', '.json')}")
